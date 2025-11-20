e918aaa35afa3fd1772265a3b7e251ea43ff9ed7
Abdullah Chaudhry <abdchaudry1205@gmail.com>
2025-11-20 22:57:36 +0500

    Create task04.cpp

---
+++ 24k-0513_lab10_tasks/task04.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void minHeapify(vector<int>& heap, int i) {
+    int n = heap.size();
+    int smallest = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+    if(l < n && heap[l] < heap[smallest]) smallest = l;
+    if(r < n && heap[r] < heap[smallest]) smallest = r;
+    if(smallest != i) {
+        swap(heap[i], heap[smallest]);
+        minHeapify(heap, smallest);
+    }
+}
+
+void insertMinHeap(vector<int>& heap, int val) {
+    heap.push_back(val);
+    int i = heap.size() - 1;
+    while(i != 0 && heap[(i-1)/2] > heap[i]) {
+        swap(heap[i], heap[(i-1)/2]);
+        i = (i-1)/2;
+    }
+}
+
+void removeMinHeap(vector<int>& heap) {
+    if(heap.empty()) return;
+    heap[0] = heap.back();
+    heap.pop_back();
+    minHeapify(heap, 0);
+}
+
+void printHeap(vector<int>& heap) {
+    for(int x : heap) cout << x << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<int> heap = {50, 60, 75, 100};
+    insertMinHeap(heap, 55);
+    printHeap(heap);
+    removeMinHeap(heap);
+    printHeap(heap);
+}
